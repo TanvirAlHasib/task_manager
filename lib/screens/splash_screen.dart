c7de4cb76c80113361ab330fbe4de96d900e5229
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-27 23:32:30 +0600

    added transparent logo

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/screens/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,10 +9,38 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    loadNextScreen();
+    super.initState();
+  }
+
+  Future loadNextScreen() async{
+    await Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen(),));
+    },);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: AlignmentGeometry.topCenter,
+            end: AlignmentGeometry.bottomCenter,
+            colors: const [
+              Color(0XFF0D1320),
+              Color(0XFF151B2A)
+            ],
+            tileMode: TileMode.mirror
+          )
+        ),
+        child: Center(
+          child: Image.asset("lib/assets/logo/logo.png"),
+        ),
+      ),
     );
   }
 }
