b56a56de157f94cfa986b61ae43929cdb132bf08
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-28 17:21:23 +0600

    needed screens init

---
+++ lib/screens/email_address_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class EmailAddressScreen extends StatefulWidget {
+  const EmailAddressScreen({super.key});
+
+  @override
+  State<EmailAddressScreen> createState() => _EmailAddressScreenState();
+}
+
+class _EmailAddressScreenState extends State<EmailAddressScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
---
+++ lib/screens/set_pass_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SetPassScreen extends StatefulWidget {
+  const SetPassScreen({super.key});
+
+  @override
+  State<SetPassScreen> createState() => _SetPassScreenState();
+}
+
+class _SetPassScreenState extends State<SetPassScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
