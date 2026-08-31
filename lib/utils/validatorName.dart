c5a5e920951bb8abd2fdd3b6c8088ebc5a8ddf44
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-31 17:16:48 +0600

    added validator name utils

---
+++ lib/utils/validatorName.dart
@@ -0,0 +1,6 @@
+class Validatorname {
+  static final String email = "email";
+  static final String pass = "password";
+  static final String otp = "otp";
+  static final String mobile = "mobile";
+}
