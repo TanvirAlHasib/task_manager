d4834c45c37d50d7bfa198b1db3f9758861231a5
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-01 16:31:51 +0600

    app bar design done

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/utils/colours.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -11,7 +12,42 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(Colours.cardColor),
+      appBar: myAppBar(),
+    );
+  }
 
+  // appbar start here
+  AppBar myAppBar(){
+    return AppBar(
+      backgroundColor: Color(Colours.backGroundColor),
+      title: Row(
+        mainAxisSize: .min,
+        spacing: 8,
+        children: [
+          ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(50),
+              child: Image.asset("lib/assets/logo/logo_starDust.png", height: 48,)
+          ),
+          Column(
+            mainAxisAlignment:.center,
+            crossAxisAlignment: .start,
+            children: [
+              Text("Welcome back,", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Color(Colours.fontColor)
+              ),),
+              Text("Tanvir Al Hasib", style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Color(0XFFA1B9F0),
+                  fontWeight: FontWeight.w600
+              ),),
+            ],
+          )
+        ],
+      ),
+      actions: [
+        Icon(Icons.notifications, color: Color(Colours.fontColor),)
+      ],
+      actionsPadding: const EdgeInsets.only(right: 13),
     );
   }
 }
