a5d78d5aa61919191039429ccedce1e293289e81
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-07 19:34:13 +0600

    added userToken static variable to use it in header

--- lib/controllers/authController.dart
+++ lib/controllers/authController.dart
@@ -2,10 +2,14 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
+  static String ? userToken;
   // save user data
   static Future saveUserData(String token, Map<String, dynamic> user) async{
     SharedPreferences preferences = await SharedPreferences.getInstance();
     preferences.setString("token", token);
     preferences.setString("user", jsonEncode(user));
+
+    // storing token if userToken  is null
+    userToken ??= preferences.getString("token");
   }
 }
