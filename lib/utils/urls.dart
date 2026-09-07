e2f5487c9c453149e9520c2954f665b55ef1a282
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-07 19:15:28 +0600

    added urls

--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -2,4 +2,8 @@ class Urls {
   static String baseUrl = "https://task-manager-api.ostad.live/api/v1";
   static String loginUrl = "$baseUrl/Login";
   static String registrationUrl = "$baseUrl/Registration";
+  static String createTaskUrl = "$baseUrl/createTask";
+  static String taskStatusCountUrl = "$baseUrl/taskStatusCount";
+  static String listTaskByStatus(String value) => "$baseUrl/listTaskByStatus/$value";
+  static String deleteTask(String id) => "$baseUrl/deleteTask/$id";
 }
