30dd3405bd8c5f6d07f1cd746e5a5ad5aae07985
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-07 09:03:47 +0600

    url util init

---
+++ lib/utils/urls.dart
@@ -0,0 +1,5 @@
+class Urls {
+  static String baseUrl = "https://task-manager-api.ostad.live/api/v1";
+  static String loginUrl = "$baseUrl/Login";
+  static String registrationUrl = "$baseUrl/Registration";
+}
