5667b42468fa81638d3198578fbac712ab00745b
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-10 16:56:06 +0600

    added bottom navigation bar

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -83,7 +83,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(Colours.cardColor),
-      appBar: myAppBar(),
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
         child: Column(
@@ -369,40 +368,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
         child: Icon(Icons.add_task, color: Colors.black,),
       ),
 
-    );
-  }
-
-  // appbar start here
-  AppBar myAppBar(){
-    return AppBar(
-      backgroundColor: Color(Colours.backGroundColor),
-      title: Row(
-        mainAxisSize: .min,
-        spacing: 8,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadiusGeometry.circular(50),
-            child: Image.asset("lib/assets/logo/logo_starDust.png", height: 48,)
-          ),
-          Column(
-            mainAxisAlignment:.center,
-            crossAxisAlignment: .start,
-            children: [
-              Text("Welcome back,", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  color: Color(Colours.fontColor)
-              ),),
-              Text("Tanvir Al Hasib", style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                  color: Color(0XFFA1B9F0),
-                  fontWeight: FontWeight.w600
-              ),),
-            ],
-          )
-        ],
-      ),
-      actions: [
-        IconButton(onPressed: () {  }, icon: Icon(Icons.notifications, color: Color(Colours.fontColor)),)
-      ],
-      actionsPadding: const EdgeInsets.only(right: 2),
     );
   }
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/screens/dashboard_screen.dart';
+import 'package:task_manager/screens/profile_screen.dart';
+
+import '../utils/colours.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +12,76 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final List<Widget> screenList = [DashboardScreen(), ProfileScreen()];
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(Colours.cardColor),
+      appBar: myAppBar(),
+      body: screenList[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(Colours.backGroundColor),
+        elevation: 1,
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        selectedItemColor: Color(Colours.fontColor),
+        showUnselectedLabels: false,
+        unselectedItemColor: Color(Colours.secondaryFontColor),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard_outlined),
+            activeIcon: Icon(Icons.dashboard),
+            label: "Dashboard"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_2_outlined),
+            activeIcon: Icon(Icons.person),
+            label: "Profile"
+          )
+        ],
+      ),
+    );
+  }
+
+  // app bar
+  AppBar myAppBar(){
+    return AppBar(
+      backgroundColor: Color(Colours.backGroundColor),
+      automaticallyImplyLeading: false,
+      title: Row(
+        mainAxisSize: .min,
+        spacing: 8,
+        children: [
+          ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(50),
+              child: Image.asset("lib/assets/logo/logo_starDust.png", height: 48,)
+          ),
+          Column(
+            mainAxisAlignment:.center,
+            crossAxisAlignment: .start,
+            children: [
+              Text("Welcome back,", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Color(Colours.fontColor)
+              ),),
+              Text("Tanvir Al Hasib", style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Color(0XFFA1B9F0),
+                  fontWeight: FontWeight.w600
+              ),),
+            ],
+          )
+        ],
+      ),
+      actions: [
+        IconButton(onPressed: () {  }, icon: Icon(Icons.notifications, color: Color(Colours.fontColor)),)
+      ],
+      actionsPadding: const EdgeInsets.only(right: 2),
+    );
   }
 }
