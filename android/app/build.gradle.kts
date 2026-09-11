8c0ce3021959632ad7209e3927ae98c00362b9f8
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-11 17:38:03 +0600

    local notification plugin setup successful

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,6 +12,8 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        // for local notification
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
@@ -46,4 +48,9 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // for local notification
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
