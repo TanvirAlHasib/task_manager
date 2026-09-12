f356816eeab93a99f667e754cedb4e2ee784577b
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-12 11:58:43 +0600

    used shared pref to retrieve the token

--- lib/api/ApiInstance.dart
+++ lib/api/ApiInstance.dart
@@ -1,17 +1,25 @@
 import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:task_manager/controllers/authController.dart';
 
 class ApiInstance {
   //get request is here
   static Future<Response> getData(String url) async{
+    // we need to retrieve the the token from shared pref
+    // because we are going to use api from work manager
+    // and the work manager will work periodically while the app is closed
+    // so the static variable can be null
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    String ? token = pref.getString("token");
+
     Response response = await http.get(
       Uri.parse(url),
       headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
-        "token" : AuthController.userToken ?? "",
+        "token" : token ?? "",
       }
     );
     return response;
@@ -19,13 +27,17 @@ class ApiInstance {
 
   // post resuest is here
   static Future<Response> postData(String url, Map<String, dynamic> body) async{
+
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    String ? token = pref.getString("token");
+
     Response response = await http.post(
       Uri.parse(url),
       body: jsonEncode(body),
       headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
-        "token" : AuthController.userToken ?? "",
+        "token" : token ?? "",
       }
     );
     return response;
