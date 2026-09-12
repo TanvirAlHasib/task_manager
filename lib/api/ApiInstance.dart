5b7122962b4ccc5545be4d5e4703dd024f2c832a
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-12 11:59:18 +0600

    used shared pref to retrieve the token

--- lib/api/ApiInstance.dart
+++ lib/api/ApiInstance.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:task_manager/controllers/authController.dart';
 
 class ApiInstance {
   //get request is here
