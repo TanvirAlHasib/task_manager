0e767a564cd4f8d590658aa859db58402c8eaa67
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-12 08:39:45 +0600

    fixed a major bug in login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:task_manager/api/ApiInstance.dart';
 import 'package:task_manager/controllers/authController.dart';
-import 'package:task_manager/screens/dashboard_screen.dart';
 import 'package:task_manager/screens/email_address_screen.dart';
+import 'package:task_manager/screens/home_screen.dart';
 import 'package:task_manager/screens/signup_screen.dart';
 import 'package:task_manager/utils/colours.dart';
 import 'package:task_manager/utils/urls.dart';
@@ -97,7 +97,7 @@ class _LoginScreenState extends State<LoginScreen> {
                               final data = jsonDecode(response.body);
                               await AuthController.saveUserData(data["token"], data["data"]);
                               Toast.show(message: "Log in successful", context: context);
-                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => DashboardScreen(),));
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen(),));
                             } else {
                               Toast.show(message: "Log in unsuccessful!!", context: context);
                             }
