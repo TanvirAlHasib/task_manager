26158f8d0a5bf8fd40c093b84c17e5f1a9d0bf01
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-12 09:06:27 +0600

    show notification periodically event added successfully

--- lib/utils/notification_helper.dart
+++ lib/utils/notification_helper.dart
@@ -1,13 +1,34 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:task_manager/utils/notification_constraints.dart';
 
 class NotificationHelper {
   static final FlutterLocalNotificationsPlugin notification = FlutterLocalNotificationsPlugin();
 
-  static void initialize() async{
+  // notification init
+  static Future<void> initialize() async{
     await notification.initialize(settings: InitializationSettings(
       android: AndroidInitializationSettings("@mipmap/ic_launcher"),
       iOS: DarwinInitializationSettings()
     ));
+  }
+
+  // notification show periodically
+  static Future<void> showNotificationPeriodically(String title, String body) async{
+    await notification.periodicallyShow(
+        id: NotificationConstraints.notificationId,
+        title: title,
+        body: body,
+        repeatInterval: RepeatInterval.daily,
+        notificationDetails: NotificationDetails(
+          android: AndroidNotificationDetails(
+            NotificationConstraints.channelId,
+            NotificationConstraints.channelName,
+            importance: Importance.high,
+            priority: Priority.high
+          )
+        ),
+        androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle
+    );
   }
 
 }
